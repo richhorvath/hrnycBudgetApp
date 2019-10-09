9fa93ce85496043a8a9960f694917fefea7c9dbe
richard <richard.horvath001@gmail.com>
2019-10-09 14:47:58 -0400

    updated accounts model function names for clarity

--- server/controllers/accounts.js
+++ server/controllers/accounts.js
@@ -1,44 +1,49 @@
-const {get, add, update, remove} = require("../models/accounts.js");
+const {
+  getCategories,
+  addCategory,
+  updateCategory,
+  removCategory
+} = require("../models/accounts.js");
 
 module.exports = {
   add: (req, res) => {
-    add(req)
-    .then(() => {
-      res.sendStatus(200);
-    })
-    .catch((error) => {
-      console.log("error in account add controller: ", error);
-      res.sendStatus(500);
-    })
+    addCategory(req)
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch(error => {
+        console.log("error in account add controller: ", error);
+        res.sendStatus(500);
+      });
   },
   get: (req, res) => {
-    get()
-    .then((data) => {
-      res.send(data)
-    })
-    .catch((error) => {
-      console.log('error in account get controller: ', error);
-      res.sendStatus(500)
-    })
+    getCategories()
+      .then(data => {
+        res.send(data);
+      })
+      .catch(error => {
+        console.log("error in account get controller: ", error);
+        res.sendStatus(500);
+      });
   },
   update: (req, res) => {
-    update(req)
-    .then(() => {
-      res.sendStatus(200)
-    })
-    .catch((error) => {
-      console.log('error in account update controller: ', error);
-      res.sendStatus(500)
-    })
+    updateCategory(req)
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch(error => {
+        console.log("error in account update controller: ", error);
+        res.sendStatus(500);
+      });
   },
   remove: (req, res) => {
-    remove(req)
-    .then(() => {
-      res.sendStatus(200)
-    })
-    .catch((error) => {
-      console.log('error in account remove controller: ', error);
-      res.sendStatus(500)
-    })
+    removCategory(req)
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch(error => {
+        console.log("error in account remove controller: ", error);
+        res.sendStatus(500);
+      });
   }
 };
--- server/models/accounts.js
+++ server/models/accounts.js
@@ -1,27 +1,33 @@
-const {connection} = require('../../db/index');
-const Promise = require('bluebird');
-const SqlString = require('sqlstring');
+const { connection } = require("../../db/index");
+const Promise = require("bluebird");
+const SqlString = require("sqlstring");
 const queryPromise = Promise.promisify(connection.query).bind(connection);
 
 module.exports = {
-    get: () => {
-        let query = "SELECT * FROM accounts";
-        return queryPromise(query);
-    },
+  getCategories: () => {
+    let query = "SELECT * FROM accounts";
+    return queryPromise(query);
+  },
 
-    add: (req) => {
-        let query = SqlString.format("INSERT INTO accounts SET ?", req.body);
-        return queryPromise(query);
-    },
+  addCategory: req => {
+    let query = SqlString.format("INSERT INTO accounts SET ?", req.body);
+    return queryPromise(query);
+  },
 
-    update: (req) => {
-        let updates = [req.body.updateName,  req.body.updateValue, req.body.id]
-        let query = SqlString.format("UPDATE accounts SET ??=? WHERE id=?", updates); 
-        return queryPromise(query);
-    },
+  updateCategory: req => {
+    let updates = [req.body.updateName, req.body.updateValue, req.body.id];
+    let query = SqlString.format(
+      "UPDATE accounts SET ??=? WHERE id=?",
+      updates
+    );
+    return queryPromise(query);
+  },
 
-    remove: (req) => {
-        let query = SqlString.format("DELETE FROM accounts WHERE id=?", req.body.id);
-        return queryPromise(query);
-    }
-}
+  removCategorye: req => {
+    let query = SqlString.format(
+      "DELETE FROM accounts WHERE id=?",
+      req.body.id
+    );
+    return queryPromise(query);
+  }
+};
