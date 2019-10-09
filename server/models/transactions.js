51ba1f9fc93cfb0172b35cb78c7b8d01b08dd91f
richard <richard.horvath001@gmail.com>
2019-10-09 14:25:58 -0400

    added account update to transaction insert query

--- server/models/transactions.js
+++ server/models/transactions.js
@@ -1,27 +1,44 @@
-const {connection} = require('../../db/index');
-const Promise = require('bluebird');
-const SqlString = require('sqlstring');
+const { connection } = require("../../db/index");
+const Promise = require("bluebird");
+const SqlString = require("sqlstring");
 const queryPromise = Promise.promisify(connection.query).bind(connection);
 
 module.exports = {
-    get: () => {
-        let query = "SELECT * FROM transact";
-        return queryPromise(query);
-    },
+  get: () => {
+    let query = "SELECT * FROM transact";
+    return queryPromise(query);
+  },
 
-    add: (req) => {
-        let query = SqlString.format("INSERT INTO transact SET ?", req.body);
-        return queryPromise(query);
-    },
+  add: req => {
+    let promises = [];
+    let transactionQuery = SqlString.format(
+      "INSERT INTO transact SET ?",
+      req.body
+    );
+    let accountUpdate = SqlString.format(
+      "UPDATE accounts SET accounts.total= accounts.total + ? WHERE accounts.id = ? ",
+      [req.body.amount, req.body.account_id]
+    );
+    promises.push(queryPromise(transactionQuery));
+    promises.push(queryPromise(accountUpdate));
 
-    update: (req) => {
-        let updates = [req.body.updateName,  req.body.updateValue, req.body.id]
-        let query = SqlString.format("UPDATE transact SET ??=? WHERE id=?", updates); 
-        return queryPromise(query);
-    },
+    return Promise.all(promises);
+  },
 
-    remove: (req) => {
-        let query = SqlString.format("DELETE FROM transact WHERE id=?", req.body.id);
-        return queryPromise(query);
-    }
-}
+  update: req => {
+    let updates = [req.body.updateName, req.body.updateValue, req.body.id];
+    let query = SqlString.format(
+      "UPDATE transact SET ??=? WHERE id=?",
+      updates
+    );
+    return queryPromise(query);
+  },
+
+  remove: req => {
+    let query = SqlString.format(
+      "DELETE FROM transact WHERE id=?",
+      req.body.id
+    );
+    return queryPromise(query);
+  }
+};
